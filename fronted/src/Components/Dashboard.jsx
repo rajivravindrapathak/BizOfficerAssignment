c3a85b0f209e6bbe8d3c57aa55208ab82d54d950
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-02-05 23:11:16 +0530

    Add files via upload

--- fronted/src/Components/Dashboard.jsx
+++ fronted/src/Components/Dashboard.jsx
@@ -1,17 +1,58 @@
 import React from "react";
 import "./Dashboard.module.css"
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line } from 'recharts';
+
 
 const Dashboard = () => {
 
     const data = [
-        {name: "Active", value: "9"},
-        {name: "Experied", value: "13"},
-        {name: "Trial", value: "0"},
-        {name: "Non Renewing", value: "2"},
-        {name: "Nuturing", value: "1"},
-        {name: "Cancelled", value: "1"}
+        {name: "Active", count: "9"},
+        {name: "Experied", count: "13"},
+        {name: "Trial", count: "0"},
+        {name: "Non Renewing", count: "2"},
+        {name: "Nuturing", count: "1"},
+        {name: "Cancelled", count: "1"}
     ]
+
+    const data1 = [
+        {
+          name: 'Active',
+          uv: 3000,
+          pv: 3000,
+          amt: 3000,
+        },
+        {
+          name: 'Experied',
+          uv: 3000,
+          pv: 3000,
+          amt: 3000,
+        },
+        {
+          name: 'Trial',
+          uv: 3000,
+          pv: 3000,
+          amt: 2290,
+        },
+        {
+          name: 'Non Renewing',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Nuturing',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Cancelled',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        }
+      ];
 
     return ( 
         <div>
@@ -173,30 +214,94 @@ const Dashboard = () => {
             </div>
 
             <div className="secmainDiv">
-                <div>
-                    <BarChart
-                        width={500}
-                        height={300}
-                        data={data}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                        }}
-                        barSize={20}
-                        >
-                        <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <Bar dataKey="value" fill="#8884d8" background={{ fill: '#eee' }} />
-                    </BarChart>
+                <div className="bar1">
+                    <div>
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                            barSize={20}
+                            >
+                            <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Bar dataKey="count" fill="#8884d8" background={{ fill: '#eee' }} />
+                        </BarChart>
+                    </div>
+                    <div>
+                        <LineChart
+                            width={500}
+                            height={300}
+                            data={data1}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                            >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
+                        </LineChart>
+                    </div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="bar1">
+                    <div>
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                            barSize={20}
+                            >
+                            <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Bar dataKey="count" fill="#8884d8" background={{ fill: '#eee' }} />
+                        </BarChart>
+                    </div>
+                    <div>
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                            barSize={20}
+                            >
+                            <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Bar dataKey="count" fill="#8884d8" background={{ fill: '#eee' }} />
+                        </BarChart>
+                    </div>
+                </div>
             </div>
 
         </div>
