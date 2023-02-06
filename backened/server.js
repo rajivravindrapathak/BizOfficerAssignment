2f45ba0d91342a535c7d6f03ce1fa8cb190010bd
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-02-06 07:16:13 +0530

    Add files via upload

--- backened/server.js
+++ backened/server.js
@@ -1,1 +1,19 @@
+const express = require("express")
+require("dotenv").config()
 
+const app = express()
+const PORT = process.env.PORT || 6000
+
+app.get((req, res) => {
+    res.send("hello")
+})
+
+app.listen(PORT, async () => {
+    try {
+        console.log("connection ok to database")
+    } catch (err) {
+        console.log("not connected to database")
+        console.log(err)
+    }
+    console.log(`listining on port ${PORT}`)
+})
