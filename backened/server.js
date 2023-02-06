88354e4d9ebba15db47da0a14195a305987b6f1f
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-02-06 08:38:08 +0530

    Add files via upload

--- backened/server.js
+++ backened/server.js
@@ -1,15 +1,21 @@
 const express = require("express")
+const {connection} = require("./config/db")
+const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 6000 
+
+app.use(cors())
+app.use(express.json())
 
 app.get((req, res) => {
-    res.send("hello")
+    res.send("hello bcrOfficer")
 })
 
 app.listen(PORT, async () => {
     try {
+        await connection
         console.log("connection ok to database")
     } catch (err) {
         console.log("not connected to database")
